fccfd9238d45cb34f28018525c720ee091e97848
Owen Shepherd <414owen@gmail.com>
2016-11-06 22:52:36 +0000

    Corrected typos, deleted obvious comments, enfored 1tbs, deleted newlines, made turn count recursive instead of static, made newlines in printf() consistent, aesthetic changes

--- Tower.c
+++ Tower.c
@@ -1,62 +1,40 @@
-/*--An algorithm that solves the Tower of Hanoi with the use of recursion
---Robert Vaughan
---15/02/2016
-*/
+/*
+ *
+ * An algorithm that solves the Tower of Hanoi with the use of recursion
+ * Robert Vaughan
+ * 15/02/2016
+ *
+ */
 
 #include <stdio.h>
 #define SIZE 1
-
-//Protoype
 int moveTower(int, int, int, int);
 
 int main()
 {
-	//Declaration
-	int input=0;
-	int i=0;
-	
-	while (input < SIZE)
-	{
-		printf("Please enter the number of discs you would like within the tower:\n");
+	int input = 0;
+	while (input < SIZE) {
+		printf("Please enter the number of discs you would like in the tower:\n");
 		scanf("%d", &input);
-		
-		if (input < SIZE)
-		{
-			printf("You must enter a number of discs great than zero!\n");
-		}//end if
-		
-	}//end while
-		
-	//Call function moveTower to begin recursion
-	i = moveTower(input, 1, 3, 2);
-
-	printf("\n\nYour tower is now complete!");
-	printf("\nThe number of movies is %d", i);
-	
-}//end main
+		if (input < SIZE) {
+			printf("You must enter a number of discs greater than one!\n");
+		}
+	}
+	int moves = moveTower(input, 1, 3, 2);
+	printf("Your tower is now complete!\n");
+	printf("The number of moves is %d\n", moves);
+}
 
 int moveTower(int disc, int source, int dest, int spare)
 {
-	//Declaration
-	static int i=0;
-	
-	i++;
-	
-	if (disc == 1)
-	{
-		printf("\nMove a disc from Tower %d to tower %d", source, dest);
-		return i;
-	}//end if
-	
-	else
-	{
-		//Recursive Call
-		moveTower(disc - 1, source, spare, dest);
-			
-		printf("\nMove a disc from Tower %d to tower %d", source, dest);
-		
-		//Recursive Call
-		moveTower(disc - 1, spare, dest, source);
-	}//end else
-	return 0;
-}//end tower()
+	if (disc == 1) {
+		printf("Moved a disc from Tower %d to tower %d\n", source, dest);
+		return 1;
+	} else {
+		int moves = 0;
+		moves += moveTower(disc - 1, source, spare, dest);
+		moves += moveTower(1, source, dest, spare);
+		moves += moveTower(disc - 1, spare, dest, source);
+		return moves;
+	}
+}
